6aad00eba191c36930c9ff73fa36f9c9f5339224
Dharshan Sai K <200949539+Dharshansai-k@users.noreply.github.com>
2026-05-11 11:42:24 +0530

    Fixed Server for Deployment

--- backend/server.js
+++ backend/server.js
@@ -4,38 +4,53 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.js";
-
 import resultRoutes from "./routes/result.js";
-
 import examRoutes from "./routes/exam.js";
-
 
 dotenv.config();
 
 const app = express();
 
-// middleware
+
+// ✅ Middleware
 app.use(cors());
+
 app.use(express.json());
 
-// routes
+
+// ✅ Routes
 app.use("/api/auth", authRoutes);
-//result routes
+
 app.use("/api/results", resultRoutes);
-// test route
+
+app.use("/api/exams", examRoutes);
+
+
+// ✅ Test Route
 app.get("/", (req, res) => {
   res.send("MockU API running 🚀");
 });
 
-//exam route
-app.use("/api/exams", examRoutes);
 
-// DB connection
-mongoose.connect(process.env.MONGO_URI)
+// ✅ PORT
+const PORT = process.env.PORT || 5000;
+
+
+// ✅ MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
+
     console.log("MongoDB connected ✅");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on ${process.env.PORT}`)
-    );
+
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+
   })
-  .catch(err => console.log(err));
+  .catch((err) => {
+
+    console.log("MongoDB Error ❌");
+    console.log(err);
+
+  });
